90d945c50f5ea3fce5451f411ac8e95350657ee7
Rebecca Murphey <rmurphey@gmail.com>
2012-09-17 19:08:40 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+
+var app = express();
+
+app.use(express.bodyParser());
+app.use(app.router);
+
+app.get('/', function(req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
+
+app.post('/sensors', function(req, res) {
+  io.sockets.emit('sensordata', req.body.data);
+  res.end('received');
+});
+
+var http = require('http');
+var server = http.createServer(app);
+var io = require('socket.io').listen(server);
+
+server.listen(3000);
